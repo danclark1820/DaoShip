8fd1e56cd2da311755f973116613a8660eff3652
danclark1820 <danclark1820@gmail.com>
2016-03-14 20:32:19 -0400

    create the high score object and file manager

---
+++ DaoShip/HighScore.swift
@@ -0,0 +1,29 @@
+//
+//  HighScore.swift
+//  DaoShip
+//
+//  Created by Daniel Clark on 3/14/16.
+//  Copyright © 2016 Daniel Clark. All rights reserved.
+//
+import Foundation
+
+class HighScore: NSObject, NSCoding {
+    let score:Int;
+    let dateOfScore:NSDate;
+    
+    init(score:Int, dateOfScore:NSDate) {
+        self.score = score;
+        self.dateOfScore = dateOfScore;
+    }
+    
+    required init(coder: NSCoder) {
+        self.score = coder.decodeObjectForKey("score")! as! Int;
+        self.dateOfScore = coder.decodeObjectForKey("dateOfScore")! as! NSDate;
+        super.init()
+    }
+    
+    func encodeWithCoder(coder: NSCoder) {
+        coder.encodeObject(self.score, forKey: "score")
+        coder.encodeObject(self.dateOfScore, forKey: "dateOfScore")
+    }
+}
